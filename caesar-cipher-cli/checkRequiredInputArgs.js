31297a3456c3fdf478a90dc4605a0c95d6ab1667
Furs Aleksey <wiemrid@gmail.com>
2020-03-29 23:57:06 +0300

    hot fix!

--- caesar-cipher-cli/checkRequiredInputArgs.js
+++ caesar-cipher-cli/checkRequiredInputArgs.js
@@ -1,9 +1,23 @@
 module.exports = function (args) {
   try {
-    if (!args.get('shift')) throw new Error('-shift parameter required');
-    if (!args.get('action')) throw new Error('-action parameter required');
-    if (isNaN(parseInt(args.get('shift')))) throw new Error('Incorrect -shift parameter');
-    if (args.get('action') !== 'encode' && args.get('action') !== 'decode') throw new Error('Incorrect -action parameter')
+    if (!args.get('shift')) {
+      throw new Error('-shift parameter required');
+    }
+    if (!args.get('action')) {
+      throw new Error('-action parameter required');
+    }
+    if (isNaN(parseInt(args.get('shift')))) {
+      throw new Error('Incorrect -shift parameter');
+    }
+    if (args.get('action') !== 'encode' && args.get('action') !== 'decode') {
+      throw new Error('Incorrect -action parameter');
+    }
+    if (!args.has('input')) {
+      args.set('input', '');
+    }
+    if (!args.has('output')) {
+      args.set('output', '');
+    }
   } catch (e) {
     console.error(`Error: ${e.message}`);
   }
